2c5397a78a5a193299447f61b72da9b6a42fca7a
d0rianb <dorian.beauchesne@icloud.com>
2024-09-24 15:13:53 +0200

    Update build process

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# install the dependencies
+git submodule update --init 
+npm install 
+
+# Build the dependencies
+cd ./lib/GaussianSplats3D/
+npm install
+npm run build-library
+cd ../..
+
+# Build the viewer
+vite build
